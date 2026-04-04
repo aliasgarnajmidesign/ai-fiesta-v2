2c61dbf662adbcedc580c4d4ab49ac4bdeef4660
aliasgarnajmidesign <aliasgarnajmi59@gmail.com>
2026-04-04 12:58:07 +0400

    Create index.ts

---
+++ lib/agents/index.ts
@@ -0,0 +1,4 @@
+export { researchAgent } from "./research";
+export { estimationAgent } from "./estimation";
+export { designAgent } from "./design";
+export { validationAgent } from "./validation";
